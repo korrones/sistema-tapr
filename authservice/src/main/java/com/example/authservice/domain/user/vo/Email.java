a597314f064da191b476d2c7c06c559c4b989068
Karolayne <karolaynedsa16@gmail.com>
2025-09-09 10:57:23 -0300

    feat: aula 1

---
+++ authservice/src/main/java/com/example/authservice/domain/user/vo/Email.java
@@ -0,0 +1,30 @@
+package com.example.authservice.domain.user.vo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+
+@Getter
+@Embeddable
+public class Email {
+
+    @jakarta.validation.constraints.Email(message = "Email invalido")
+    @Column(name = "email", nullable = false, unique = true)
+    private String value;
+
+    public Email(String value) {
+        this.value = normalize(value);
+
+        if (this.value == null || this.value.isBlank()) {
+            throw new IllegalArgumentException("Email não pode ser em branco");
+        }
+    }
+
+    public static Email of(String value) {
+        return new Email(value);
+    }
+
+    private static String normalize(String value) {
+        return value == null ? null : value.trim().toLowerCase();
+    }
+}
